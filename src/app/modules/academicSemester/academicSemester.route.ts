1ede4ef1d307ae988cf89d829641419633f65587
Niloy Roy <niloyroy184@gmail.com>
2024-01-30 23:45:50 +0600

    solved server adding again

---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterControllers } from './academicSemester.controller';
+import { AcademicSemesterValidations } from './academicSemester.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-semester',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  validateRequest(
+    AcademicSemesterValidations.createAcdemicSemesterValidationSchema,
+  ),
+  AcademicSemesterControllers.createAcademicSemester,
+);
+
+router.get('/:courseId', AcademicSemesterControllers.getSingleAcademicSemester);
+
+router.patch(
+  '/:courseId',
+  auth('admin'),
+  validateRequest(
+    AcademicSemesterValidations.updateAcademicSemesterValidationSchema,
+  ),
+  AcademicSemesterControllers.updateAcademicSemester,
+);
+
+router.get(
+  '/',
+  auth('admin'),
+  AcademicSemesterControllers.getAllAcademicSemesters,
+);
+
+export const AcademicSemesterRoutes = router;
